bb2e2acfe366e267bfb4585005ace826520c678f
Cyril Tobler <cyriltobler@icloud.com>
2024-02-07 08:02:06 +0000

    chore: fixed bugs in the verification-middleware

--- app/game/game-routes.js
+++ app/game/game-routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const verification = require('../middleware/verification-middleware.js')
+
 const VIEW_PATH = '../../view/';
 
-router.get('/game', (req, res) => {
+router.get('/game', verification.checkIfAuthenticated, (req, res) => {
     const filePath = path.join(__dirname, VIEW_PATH, '/pages/game.ejs');
     res.render(filePath);
 });
--- app/middleware/verification-middleware.js
+++ app/middleware/verification-middleware.js
@@ -1,18 +1,17 @@
-
-
 const checkIfNotAuthenticated = (req, res, next) => {
     if(req.isAuthenticated()) {
-        return res.redirect('/')
+        return res.redirect('/');
     }
-    next()
+    next();
 }
+
 const checkIfAuthenticated = (req, res, next) => {
     if(!req.isAuthenticated()) {
-        res.redirect('/account/login')
+        return res.redirect('/auth/login');
     }
-    next()
+    next();
 }
 
 
-module.exports.checkAuthenticated = checkIfAuthenticated
-module.exports.checkIfNotAuthenticated = checkIfNotAuthenticated
+module.exports.checkIfAuthenticated = checkIfAuthenticated;
+module.exports.checkIfNotAuthenticated = checkIfNotAuthenticated;
