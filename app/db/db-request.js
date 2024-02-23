f22be494cb03017d1a884f74f8ff6cd445f0ff95
Cyril Tobler <cyriltobler@icloud.com>
2024-02-23 08:08:34 +0000

    fix: Server crash by wrong DB hostname

--- app/db/db-request.js
+++ app/db/db-request.js
@@ -4,7 +4,7 @@ const pool = require('./db-config');
 const dbRequest = (query, data, callback) => {
     pool.getConnection((error, connection) => {
         if (error) {
-            callback(false, error);
+            return callback(false, error);
         }
 
         connection.query(query, data, async (queryError, results) => {
@@ -12,9 +12,9 @@ const dbRequest = (query, data, callback) => {
             connection.release();
 
             if (queryError) {
-                callback(false, queryError);
+                return callback(false, queryError);
             }
-            callback(true, results);
+            return callback(true, results);
         });
     });
 };
